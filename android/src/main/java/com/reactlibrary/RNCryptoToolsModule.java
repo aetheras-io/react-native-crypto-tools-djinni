c8c601691ff93c8e1ee626948a4921fc79e14248
Denis Tsai <dt665m@gmail.com>
2019-02-23 01:22:39 -0800

    initial working java c++ bridge

--- android/src/main/java/com/reactlibrary/RNCryptoToolsModule.java
+++ android/src/main/java/com/reactlibrary/RNCryptoToolsModule.java
@@ -1,17 +1,28 @@
 package com.reactlibrary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import io.aetheras.djinni.GreetingService;
+
 public class RNCryptoToolsModule extends ReactContextBaseJavaModule {
+    static {
+        System.loadLibrary("core-native");
+    }
 
     private final ReactApplicationContext reactContext;
+    private final GreetingService greetingService;
 
     public RNCryptoToolsModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        this.greetingService = GreetingService.createService();
     }
 
     @Override
@@ -20,7 +31,11 @@ public class RNCryptoToolsModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+    public void whatsup(String stringArgument, Callback callback) {
         // TODO: Implement
+        Log.i("RNCryptoToolsModule", "Argument:" + stringArgument);
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(stringArgument.split(",")));
+        Log.i("RNCryptoToolsModule-R",greetingService.greet(list));
+        callback.invoke(greetingService.greet(list));
     }
 }
